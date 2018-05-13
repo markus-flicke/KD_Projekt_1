732bbc0e092cabd4ec3eef644ba81e0b4e3ed7c7
m <mark.alex@web.de>
2018-05-13 14:57:31 +0200

    log transform example

--- Scripts/1.R
+++ Scripts/1.R
@@ -2,16 +2,23 @@ library(dbt.DataIO)
 library(DbtFunc)
 library(pareto)
 library(AdaptGauss)
+
+library(DataVisualizations)
+library(dbt.Statistics)
+
+library(dbt.Plot)
 #setwd(paste(getwd(),"/../", sep =""))
 
 aml_raw <- ReadLRN('./Data/13AMLKHd10')
 healthy_raw <- ReadLRN('./Data/13PKd10')
 
-data <- healthy_raw$Data
-header <- healthy_raw$Header
+data <- aml_raw$Data
+header <- aml_raw$Header
 
 scale_adjusted <- apply(data, 2, function (col) {col / max(col)})
+logtransformed <- apply(data, 2, function (x) {log(x+1)})
 
-for (i in 1:length(header)){
-  PlotDistributionAnalysis(data[,i], header[i], 'Healthy')
-}
+roottransformed <- apply(data, 2, function (x) {x^0.5})
+
+dbt_qqnorm(logtransformed[,5])
+header
